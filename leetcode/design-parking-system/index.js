e0405738ba0699d68c1b67bcae380c0c1c82eea6
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-12-08 10:16:27 -0300

    add: solution to 1603. Design Parking System

---
+++ leetcode/design-parking-system/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function (big, medium, small) {
+  this.spaceAvailable = { 1: big, 2: medium, 3: small };
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function (carType) {
+  if (this.spaceAvailable[carType] > 0) {
+    this.spaceAvailable[carType]--;
+
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
+
+module.exports = {
+  ParkingSystem,
+};
---
+++ leetcode/design-parking-system/index.test.js
@@ -0,0 +1,19 @@
+const { ParkingSystem } = require(".");
+
+describe("771. Jewels and Stones", () => {
+  // test("1", () => {
+  //   const parkingSystem = new ParkingSystem(1, 1, 0);
+
+  //   expect(parkingSystem.addCar(1)).toBe(true);
+  //   expect(parkingSystem.addCar(2)).toBe(true);
+  //   expect(parkingSystem.addCar(3)).toBe(false);
+  //   expect(parkingSystem.addCar(1)).toBe(false);
+  // });
+  test("2", () => {
+    const parkingSystem = new ParkingSystem(0, 0, 2);
+
+    expect(parkingSystem.addCar(1)).toBe(false);
+    expect(parkingSystem.addCar(2)).toBe(false);
+    expect(parkingSystem.addCar(3)).toBe(true);
+  });
+});
