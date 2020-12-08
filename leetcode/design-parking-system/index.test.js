f7cc6e3832a9def5b5475ab85599af9875719b87
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-12-08 10:19:25 -0300

    fix: remove test commited

--- leetcode/design-parking-system/index.test.js
+++ leetcode/design-parking-system/index.test.js
@@ -1,14 +1,14 @@
 const { ParkingSystem } = require(".");
 
 describe("771. Jewels and Stones", () => {
-  // test("1", () => {
-  //   const parkingSystem = new ParkingSystem(1, 1, 0);
+  test("1", () => {
+    const parkingSystem = new ParkingSystem(1, 1, 0);
 
-  //   expect(parkingSystem.addCar(1)).toBe(true);
-  //   expect(parkingSystem.addCar(2)).toBe(true);
-  //   expect(parkingSystem.addCar(3)).toBe(false);
-  //   expect(parkingSystem.addCar(1)).toBe(false);
-  // });
+    expect(parkingSystem.addCar(1)).toBe(true);
+    expect(parkingSystem.addCar(2)).toBe(true);
+    expect(parkingSystem.addCar(3)).toBe(false);
+    expect(parkingSystem.addCar(1)).toBe(false);
+  });
   test("2", () => {
     const parkingSystem = new ParkingSystem(0, 0, 2);
 
